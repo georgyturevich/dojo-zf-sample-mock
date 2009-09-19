435c8f8422c32015a7686d2c90bb916f7f740037
georgy.turevich <georgy.turevich@44a4c484-a534-11de-b950-3d5b5f4ea0aa>
2009-09-19 19:29:16 +0000

    Код для получения трассировки с помощью пакета APD

--- trunk/public/index.php
+++ trunk/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+//apd_set_pprof_trace();
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
