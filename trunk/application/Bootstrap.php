28c4dea9c5b5575b56325bd726fb6059c60cb80a
georgy.turevich <georgy.turevich@44a4c484-a534-11de-b950-3d5b5f4ea0aa>
2009-09-20 19:04:26 +0000

    Внедряем autoload ресурсов модуля

--- trunk/application/Bootstrap.php
+++ trunk/application/Bootstrap.php
@@ -2,6 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    protected function _initAutoload()
+    {
+        $autoloader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => 'Default',
+            'basePath'  => dirname(__FILE__),
+        ));
+        return $autoloader;
+    }
 }
--- trunk/application/controllers/IndexController.php
+++ trunk/application/controllers/IndexController.php
@@ -11,6 +11,9 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $some = new Default_Model_Some();
+        
+        Dojend_Debug::vd($some);
     }
 
 
